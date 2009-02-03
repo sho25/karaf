10a284d117c1a83e2de44ac8f03e94aac1573648
Guillaume Nodet <gnodet@apache.org>
2009-02-03 20:36:48 +0000

    SMX4KNL-152: Change 'kernel' to 'Kernel' in the info command
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx4/kernel/trunk@740427 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c3ae61c9a491079108c74e9d8bda07479d743213

--- gshell/gshell-core/src/main/java/org/apache/servicemix/kernel/gshell/core/commands/InfoAction.java
+++ gshell/gshell-core/src/main/java/org/apache/servicemix/kernel/gshell/core/commands/InfoAction.java
@@ -502,7 +502,7 @@ argument_list|)
 expr_stmt|;
 name|printValue
 argument_list|(
-literal|"ServiceMix kernel version"
+literal|"ServiceMix Kernel version"
 argument_list|,
 name|maxNameLen
 argument_list|,
