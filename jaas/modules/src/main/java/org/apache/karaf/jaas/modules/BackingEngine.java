61da0fd403d7bea1182045a2770aa82530ab7058
Ioannis Canellos <iocanel@apache.org>
2010-10-29 14:01:14 +0000

    [KARAF-188] Added a BackingEngine and BackingEngineFactory per LoginModule.
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1028750 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: bcd47406d8032ffa942c412dc0486ffc14a7848b

---
+++ jaas/modules/src/main/java/org/apache/karaf/jaas/modules/BackingEngine.java
@@ -0,0 +1,74 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  *  Licensed under the Apache License, Version 2.0 (the "License");  *  you may not use this file except in compliance with the License.  *  You may obtain a copy of the License at  *  *       http://www.apache.org/licenses/LICENSE-2.0  *  *  Unless required by applicable law or agreed to in writing, software  *  distributed under the License is distributed on an "AS IS" BASIS,  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *  See the License for the specific language governing permissions and  *  limitations under the License.  *  under the License.  */
+end_comment
+
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|karaf
+operator|.
+name|jaas
+operator|.
+name|modules
+package|;
+end_package
+
+begin_comment
+comment|/**  * @author iocanel  */
+end_comment
+
+begin_interface
+specifier|public
+interface|interface
+name|BackingEngine
+block|{
+comment|/**      * Create a new User.      *      * @param username      * @param password      */
+name|void
+name|addUser
+parameter_list|(
+name|String
+name|username
+parameter_list|,
+name|String
+name|password
+parameter_list|)
+function_decl|;
+comment|/**      * Delete User      *      * @param username      */
+name|void
+name|deleteUser
+parameter_list|(
+name|String
+name|username
+parameter_list|)
+function_decl|;
+comment|/**      * Add a role to the user      *      * @param username      * @param role      */
+name|void
+name|addRole
+parameter_list|(
+name|String
+name|username
+parameter_list|,
+name|String
+name|role
+parameter_list|)
+function_decl|;
+comment|/**      * Remove a role from a user.      *      * @param username      * @param role      */
+name|void
+name|deleteRole
+parameter_list|(
+name|String
+name|username
+parameter_list|,
+name|String
+name|role
+parameter_list|)
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
