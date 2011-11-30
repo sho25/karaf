ebcace716bd7af462a0734458c3866e7b3d11cfe
Jean-Baptiste Onofre <jbonofre@apache.org>
2011-11-30 21:54:26 +0000

    [KARAF-963] Refactoring of the system module
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1208819 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 56554bc2d7795f26e274889a93f8bfe90469ed7b

---
+++ system/core/src/main/java/org/apache/karaf/system/SystemService.java
@@ -0,0 +1,65 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|karaf
+operator|.
+name|system
+package|;
+end_package
+
+begin_comment
+comment|/**  * Describe a system service  */
+end_comment
+
+begin_interface
+specifier|public
+interface|interface
+name|SystemService
+block|{
+comment|/**      * Shutdown the Karaf container.      */
+name|void
+name|shutdown
+parameter_list|()
+throws|throws
+name|Exception
+function_decl|;
+comment|/**      * Shutdown the Karaf container.      *      * @param time shutdown delay. The time argument can have different formats.      *  First, it can be an abolute time in the format hh:mm, in which hh is the hour (1 or 2 digits) and mm      *  is the minute of the hour (in two digits). Second, it can be in the format +m, in which m is the number of minutes      *  to wait. The word now is an alias for +0.      */
+name|void
+name|shutdown
+parameter_list|(
+name|String
+name|time
+parameter_list|)
+throws|throws
+name|Exception
+function_decl|;
+comment|/**      * Set the system start level.      *      * @param startLevel the new system start level.      */
+name|void
+name|setStartLevel
+parameter_list|(
+name|int
+name|startLevel
+parameter_list|)
+throws|throws
+name|Exception
+function_decl|;
+comment|/**      * Get the system start level.      *      * @return the current system start level.      */
+name|int
+name|getStartLevel
+parameter_list|()
+throws|throws
+name|Exception
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
