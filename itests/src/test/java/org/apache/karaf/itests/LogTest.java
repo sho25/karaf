bc01e2be5e398bf6298d7ecadacd7a0a7769e145
Jean-Baptiste Onofre <jbonofre@apache.org>
2013-12-02 18:28:43 +0000

    [KARAF-2595] Disable LogTest test via MBean waiting the new method
    git-svn-id: https://svn.apache.org/repos/asf/karaf/trunk@1547130 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 109cfdc7d057730e85b55c6e16561466eb223703

--- itests/src/test/java/org/apache/karaf/itests/LogTest.java
+++ itests/src/test/java/org/apache/karaf/itests/LogTest.java
@@ -87,6 +87,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -245,6 +255,8 @@ expr_stmt|;
 block|}
 annotation|@
 name|Test
+annotation|@
+name|Ignore
 specifier|public
 name|void
 name|setDebugViaMBean
