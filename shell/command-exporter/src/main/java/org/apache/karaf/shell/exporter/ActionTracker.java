a7104bf7666e746f209dc7e517769d67e8e08dc4
Jean-Baptiste Onofré <jbonofre@apache.org>
2014-02-22 10:55:04 +0100

    Fix legal and code style issues
    Former-commit-id: ab704c6bcb2d01d5e4de76a86ace30ed109d270d

--- shell/command-exporter/src/main/java/org/apache/karaf/shell/exporter/ActionTracker.java
+++ shell/command-exporter/src/main/java/org/apache/karaf/shell/exporter/ActionTracker.java
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *   http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing,  * software distributed under the License is distributed on an  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY  * KIND, either express or implied.  See the License for the  * specific language governing permissions and limitations  * under the License.  */
+end_comment
+
 begin_package
 package|package
 name|org
@@ -148,7 +152,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Tracks services that implement {@link org.apache.felix.gogo.commands.Action},   * wraps each into an ActionCommand   * and exports the command as a service in the name of the bundle exporting the Action  */
+comment|/**  * Tracks services that implement {@link org.apache.felix.gogo.commands.Action},  * wraps each into an ActionCommand  * and exports the command as a service in the name of the bundle exporting the Action  */
 end_comment
 
 begin_class
