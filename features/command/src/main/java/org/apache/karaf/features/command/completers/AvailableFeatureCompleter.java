1419a24f7509dd48ca24f95b97a7199f2d3e37a5
Guillaume Nodet <gnodet@gmail.com>
2015-01-08 10:00:16 +0100

    Fix javadoc link
    Former-commit-id: db1143e8966acd535d039cd2b2dbd61ffe9e2092

--- features/command/src/main/java/org/apache/karaf/features/command/completers/AvailableFeatureCompleter.java
+++ features/command/src/main/java/org/apache/karaf/features/command/completers/AvailableFeatureCompleter.java
@@ -54,7 +54,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * {@link org.apache.karaf.shell.console.Completer} for features not installed yet.  */
+comment|/**  * {@link org.apache.karaf.shell.api.console.Completer} for features not installed yet.  */
 end_comment
 
 begin_class
