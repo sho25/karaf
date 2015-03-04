f5ccf057f005534148b4d7bc600a6f7d785f3ace
Guillaume Nodet <gnodet@gmail.com>
2015-03-04 15:04:40 +0100

    Revert "Fix problem with updating snapshots"
    This reverts commit 00b2cb1c96f0c8be4aec5cd553b270f6db4605aa [formerly f4f2c91fa549d504f751f53ff5b65418a04ee163].
    
    
    Former-commit-id: 759d29cfa17185cec147ca5b1ff9b8e8daa3ddb0

--- features/core/src/main/java/org/apache/karaf/features/internal/service/FeaturesServiceImpl.java
+++ features/core/src/main/java/org/apache/karaf/features/internal/service/FeaturesServiceImpl.java
@@ -6413,12 +6413,6 @@ name|featureResolutionRange
 expr_stmt|;
 name|request
 operator|.
-name|updateSnaphots
-operator|=
-name|updateSnaphots
-expr_stmt|;
-name|request
-operator|.
 name|globalRepository
 operator|=
 name|globalRepository
