ee26481bb08cfcdb45cbb10543278332184215b6
jgoodyear <jgoodyear@apache.org>
2016-08-01 22:23:17 -0230

    [KARAF-4642] featuresBoot order is not honored. Thank you James Carman for the patch.
    Former-commit-id: e5beeb0a8174a4a9ec1d01e1b3ba569ed0675142

--- features/core/src/main/java/org/apache/karaf/features/internal/service/BootFeaturesInstaller.java
+++ features/core/src/main/java/org/apache/karaf/features/internal/service/BootFeaturesInstaller.java
@@ -95,6 +95,16 @@ name|java
 operator|.
 name|util
 operator|.
+name|LinkedHashSet
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
 name|List
 import|;
 end_import
@@ -658,7 +668,7 @@ argument_list|>
 name|features
 init|=
 operator|new
-name|HashSet
+name|LinkedHashSet
 argument_list|<>
 argument_list|()
 decl_stmt|;
