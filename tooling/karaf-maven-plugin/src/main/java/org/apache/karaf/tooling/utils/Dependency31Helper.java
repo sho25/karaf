b74d8d5af960d131519ac1bd3cb4c7255bd2e4f6
Paul Campbell <pcampbell@kemitix.net>
2017-04-12 19:36:54 +0100

    tooling.karaf-maven-plugin: Dependency30Helper removed
    Updated MavenUtilTest
    
    
    Former-commit-id: d114b1d28597dd4b0e767b6435f7016948e63a09

--- tooling/karaf-maven-plugin/src/main/java/org/apache/karaf/tooling/utils/Dependency31Helper.java
+++ tooling/karaf-maven-plugin/src/main/java/org/apache/karaf/tooling/utils/Dependency31Helper.java
@@ -428,7 +428,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  *<p>{@link DependencyHelper} for accessing Eclipse Aether system used in Maven 3.1+. It uses reflection to access  * these methods of {@code maven-core} APIs which directly references Eclipse Aether classes.</p>  *  *<p>When {@code karaf-maven-plugin} switches to {@code maven-core:3.1.0+}, reflection should be use for Sonatype  * Aether in {@link Dependency30Helper} and this class will use Maven API directly.</p>  */
+comment|/**  *<p>{@link DependencyHelper} for accessing Eclipse Aether system used in Maven 3.1+. It uses reflection to access  * these methods of {@code maven-core} APIs which directly references Eclipse Aether classes.</p>  */
 end_comment
 
 begin_class
