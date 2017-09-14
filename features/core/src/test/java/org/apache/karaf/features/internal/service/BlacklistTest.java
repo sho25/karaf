sho25/karaf

commit bc72970be35e0f62ceb4341554189b00e6bcbeea

Change Blacklist into an ordinary class
Former-commit-id: eb95bce7d5a1bcf23a5bda919209394ffcf3c01a

features/core/src/main/java/org/apache/karaf/features/internal/service/Blacklist.java
@@ -20,6 +20,20 @@ package|;
 end_package
 
 begin_import
+import|import static
+name|java
+operator|.
+name|util
+operator|.
+name|stream
+operator|.
+name|Collectors
+operator|.
+name|toSet
+import|;
+end_import
+
+begin_import
 import|import
 name|java
 operator|.
@@ -75,7 +89,47 @@ name|java
 operator|.
 name|util
 operator|.
-name|*
+name|Collections
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|HashSet
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Iterator
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|List
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Set
 import|;
 end_import
 
@@ -308,59 +362,47 @@ name|class
 argument_list|)
 decl_stmt|;
 specifier|private
+name|Clause
+index|[]
+name|clauses
+decl_stmt|;
+specifier|public
 name|Blacklist
 parameter_list|()
-block|{     }
-specifier|public
-specifier|static
-name|void
-name|blacklist
-parameter_list|(
-name|Features
-name|features
-parameter_list|,
-name|String
-name|blacklisted
-parameter_list|)
 block|{
-name|Set
-argument_list|<
-name|String
-argument_list|>
-name|blacklist
-init|=
-name|loadBlacklist
+name|this
 argument_list|(
-name|blacklisted
-argument_list|)
-decl_stmt|;
-name|blacklist
-argument_list|(
-name|features
-argument_list|,
-name|blacklist
+name|Collections
+operator|.
+name|emptyList
+argument_list|()
 argument_list|)
 expr_stmt|;
 block|}
 specifier|public
-specifier|static
-name|void
-name|blacklist
+name|Blacklist
 parameter_list|(
-name|Features
-name|features
-parameter_list|,
-name|Collection
+name|List
 argument_list|<
 name|String
 argument_list|>
 name|blacklist
 parameter_list|)
 block|{
-name|Clause
-index|[]
+name|this
+operator|.
 name|clauses
-init|=
+operator|=
+name|org
+operator|.
+name|apache
+operator|.
+name|felix
+operator|.
+name|utils
+operator|.
+name|manifest
+operator|.
 name|Parser
 operator|.
 name|parseClauses
@@ -379,26 +421,162 @@ argument_list|()
 index|]
 argument_list|)
 argument_list|)
-decl_stmt|;
-name|blacklist
-argument_list|(
-name|features
-argument_list|,
-name|clauses
-argument_list|)
 expr_stmt|;
 block|}
 specifier|public
-specifier|static
+name|Blacklist
+parameter_list|(
+name|String
+name|blacklistUrl
+parameter_list|)
+block|{
+name|Set
+argument_list|<
+name|String
+argument_list|>
+name|blacklist
+init|=
+operator|new
+name|HashSet
+argument_list|<>
+argument_list|()
+decl_stmt|;
+if|if
+condition|(
+name|blacklistUrl
+operator|!=
+literal|null
+condition|)
+block|{
+try|try
+init|(
+name|InputStream
+name|is
+init|=
+operator|new
+name|URL
+argument_list|(
+name|blacklistUrl
+argument_list|)
+operator|.
+name|openStream
+argument_list|()
+init|;                 BufferedReader reader = new BufferedReader(new InputStreamReader(is)
+block|)
+block|)
+block|{
+name|reader
+operator|.
+name|lines
+argument_list|()
+comment|//
+operator|.
+name|map
+argument_list|(
+name|line
+lambda|->
+name|line
+operator|.
+name|trim
+argument_list|()
+argument_list|)
+comment|//
+operator|.
+name|filter
+argument_list|(
+name|line
+lambda|->
+name|line
+operator|.
+name|isEmpty
+argument_list|()
+operator|||
+name|line
+operator|.
+name|startsWith
+argument_list|(
+literal|"#"
+argument_list|)
+argument_list|)
+operator|.
+name|collect
+argument_list|(
+name|toSet
+argument_list|()
+argument_list|)
+expr_stmt|;
+block|}
+catch|catch
+parameter_list|(
+name|FileNotFoundException
+name|e
+parameter_list|)
+block|{
+name|LOGGER
+operator|.
+name|debug
+argument_list|(
+literal|"Unable to load blacklist bundles list"
+argument_list|,
+name|e
+operator|.
+name|toString
+argument_list|()
+argument_list|)
+expr_stmt|;
+block|}
+catch|catch
+parameter_list|(
+name|Exception
+name|e
+parameter_list|)
+block|{
+name|LOGGER
+operator|.
+name|debug
+argument_list|(
+literal|"Unable to load blacklist bundles list"
+argument_list|,
+name|e
+argument_list|)
+expr_stmt|;
+block|}
+block|}
+end_class
+
+begin_expr_stmt
+name|this
+operator|.
+name|clauses
+operator|=
+name|Parser
+operator|.
+name|parseClauses
+argument_list|(
+name|blacklist
+operator|.
+name|toArray
+argument_list|(
+operator|new
+name|String
+index|[
+name|blacklist
+operator|.
+name|size
+argument_list|()
+index|]
+argument_list|)
+argument_list|)
+expr_stmt|;
+end_expr_stmt
+
+begin_function
+unit|}      public
 name|void
 name|blacklist
 parameter_list|(
 name|Features
 name|features
-parameter_list|,
-name|Clause
-index|[]
-name|clauses
 parameter_list|)
 block|{
 name|features
@@ -413,23 +591,19 @@ lambda|->
 name|blacklist
 argument_list|(
 name|feature
-argument_list|,
-name|clauses
 argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
+end_function
+
+begin_function
 specifier|public
-specifier|static
 name|boolean
 name|blacklist
 parameter_list|(
 name|Feature
 name|feature
-parameter_list|,
-name|Clause
-index|[]
-name|clauses
 parameter_list|)
 block|{
 for|for
@@ -548,8 +722,6 @@ name|feature
 operator|.
 name|getBundle
 argument_list|()
-argument_list|,
-name|clauses
 argument_list|)
 expr_stmt|;
 comment|// Check conditional bundles
@@ -570,8 +742,6 @@ name|cond
 operator|.
 name|getBundle
 argument_list|()
-argument_list|,
-name|clauses
 argument_list|)
 expr_stmt|;
 block|}
@@ -580,8 +750,10 @@ return|return
 literal|false
 return|;
 block|}
+end_function
+
+begin_function
 specifier|private
-specifier|static
 name|void
 name|blacklist
 parameter_list|(
@@ -590,10 +762,6 @@ argument_list|<
 name|Bundle
 argument_list|>
 name|bundles
-parameter_list|,
-name|Clause
-index|[]
-name|clauses
 parameter_list|)
 block|{
 for|for
@@ -710,169 +878,13 @@ block|}
 block|}
 block|}
 block|}
+end_function
+
+begin_function
 specifier|public
-specifier|static
-name|Set
-argument_list|<
-name|String
-argument_list|>
-name|loadBlacklist
-parameter_list|(
-name|String
-name|blacklistUrl
-parameter_list|)
-block|{
-name|Set
-argument_list|<
-name|String
-argument_list|>
-name|blacklist
-init|=
-operator|new
-name|HashSet
-argument_list|<>
-argument_list|()
-decl_stmt|;
-try|try
-block|{
-if|if
-condition|(
-name|blacklistUrl
-operator|!=
-literal|null
-condition|)
-block|{
-try|try
-init|(
-name|InputStream
-name|is
-init|=
-operator|new
-name|URL
-argument_list|(
-name|blacklistUrl
-argument_list|)
-operator|.
-name|openStream
-argument_list|()
-init|)
-block|{
-name|BufferedReader
-name|reader
-init|=
-operator|new
-name|BufferedReader
-argument_list|(
-operator|new
-name|InputStreamReader
-argument_list|(
-name|is
-argument_list|)
-argument_list|)
-decl_stmt|;
-name|String
-name|line
-decl_stmt|;
-while|while
-condition|(
-operator|(
-name|line
-operator|=
-name|reader
-operator|.
-name|readLine
-argument_list|()
-operator|)
-operator|!=
-literal|null
-condition|)
-block|{
-name|line
-operator|=
-name|line
-operator|.
-name|trim
-argument_list|()
-expr_stmt|;
-if|if
-condition|(
-operator|!
-name|line
-operator|.
-name|isEmpty
-argument_list|()
-operator|&&
-operator|!
-name|line
-operator|.
-name|startsWith
-argument_list|(
-literal|"#"
-argument_list|)
-condition|)
-block|{
-name|blacklist
-operator|.
-name|add
-argument_list|(
-name|line
-argument_list|)
-expr_stmt|;
-block|}
-block|}
-block|}
-block|}
-block|}
-catch|catch
-parameter_list|(
-name|FileNotFoundException
-name|e
-parameter_list|)
-block|{
-name|LOGGER
-operator|.
-name|debug
-argument_list|(
-literal|"Unable to load blacklist bundles list"
-argument_list|,
-name|e
-operator|.
-name|toString
-argument_list|()
-argument_list|)
-expr_stmt|;
-block|}
-catch|catch
-parameter_list|(
-name|Exception
-name|e
-parameter_list|)
-block|{
-name|LOGGER
-operator|.
-name|debug
-argument_list|(
-literal|"Unable to load blacklist bundles list"
-argument_list|,
-name|e
-argument_list|)
-expr_stmt|;
-block|}
-return|return
-name|blacklist
-return|;
-block|}
-specifier|public
-specifier|static
 name|boolean
 name|isFeatureBlacklisted
 parameter_list|(
-name|List
-argument_list|<
-name|String
-argument_list|>
-name|blacklist
-parameter_list|,
 name|String
 name|name
 parameter_list|,
@@ -880,29 +892,6 @@ name|String
 name|version
 parameter_list|)
 block|{
-name|Clause
-index|[]
-name|clauses
-init|=
-name|Parser
-operator|.
-name|parseClauses
-argument_list|(
-name|blacklist
-operator|.
-name|toArray
-argument_list|(
-operator|new
-name|String
-index|[
-name|blacklist
-operator|.
-name|size
-argument_list|()
-index|]
-argument_list|)
-argument_list|)
-decl_stmt|;
 for|for
 control|(
 name|Clause
@@ -1010,17 +999,13 @@ return|return
 literal|false
 return|;
 block|}
+end_function
+
+begin_function
 specifier|public
-specifier|static
 name|boolean
 name|isBundleBlacklisted
 parameter_list|(
-name|List
-argument_list|<
-name|String
-argument_list|>
-name|blacklist
-parameter_list|,
 name|String
 name|uri
 parameter_list|)
@@ -1028,75 +1013,19 @@ block|{
 return|return
 name|isBlacklisted
 argument_list|(
-name|blacklist
-argument_list|,
 name|uri
 argument_list|,
 name|TYPE_BUNDLE
 argument_list|)
 return|;
 block|}
+end_function
+
+begin_function
 specifier|public
-specifier|static
 name|boolean
 name|isBlacklisted
 parameter_list|(
-name|List
-argument_list|<
-name|String
-argument_list|>
-name|blacklist
-parameter_list|,
-name|String
-name|uri
-parameter_list|,
-name|String
-name|btype
-parameter_list|)
-block|{
-name|Clause
-index|[]
-name|clauses
-init|=
-name|Parser
-operator|.
-name|parseClauses
-argument_list|(
-name|blacklist
-operator|.
-name|toArray
-argument_list|(
-operator|new
-name|String
-index|[
-name|blacklist
-operator|.
-name|size
-argument_list|()
-index|]
-argument_list|)
-argument_list|)
-decl_stmt|;
-return|return
-name|isBlacklisted
-argument_list|(
-name|clauses
-argument_list|,
-name|uri
-argument_list|,
-name|btype
-argument_list|)
-return|;
-block|}
-specifier|public
-specifier|static
-name|boolean
-name|isBlacklisted
-parameter_list|(
-name|Clause
-index|[]
-name|clauses
-parameter_list|,
 name|String
 name|uri
 parameter_list|,
@@ -1186,8 +1115,8 @@ return|return
 literal|false
 return|;
 block|}
-block|}
-end_class
+end_function
 
+unit|}
 end_unit
 

features/core/src/test/java/org/apache/karaf/features/internal/service/BlacklistTest.java
@@ -20,6 +20,18 @@ package|;
 end_package
 
 begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
+operator|.
+name|assertTrue
+import|;
+end_import
+
+begin_import
 import|import
 name|java
 operator|.
@@ -35,7 +47,7 @@ name|java
 operator|.
 name|util
 operator|.
-name|ArrayList
+name|Collections
 import|;
 end_import
 
@@ -45,7 +57,9 @@ name|java
 operator|.
 name|util
 operator|.
-name|List
+name|stream
+operator|.
+name|Stream
 import|;
 end_import
 
@@ -59,11 +73,7 @@ name|karaf
 operator|.
 name|features
 operator|.
-name|internal
-operator|.
-name|model
-operator|.
-name|Bundle
+name|BundleInfo
 import|;
 end_import
 
@@ -128,30 +138,6 @@ operator|.
 name|junit
 operator|.
 name|Test
-import|;
-end_import
-
-begin_import
-import|import static
-name|org
-operator|.
-name|junit
-operator|.
-name|Assert
-operator|.
-name|assertFalse
-import|;
-end_import
-
-begin_import
-import|import static
-name|org
-operator|.
-name|junit
-operator|.
-name|Assert
-operator|.
-name|assertNotEquals
 import|;
 end_import
 
@@ -167,81 +153,37 @@ name|void
 name|testBlacklistFeatureWithRange
 parameter_list|()
 block|{
-name|URL
-name|url
-init|=
-name|getClass
-argument_list|()
-operator|.
-name|getResource
-argument_list|(
-literal|"f02.xml"
-argument_list|)
-decl_stmt|;
-name|Features
+name|Stream
+argument_list|<
+name|Feature
+argument_list|>
 name|features
 init|=
-name|JaxbUtil
-operator|.
-name|unmarshal
-argument_list|(
-name|url
-operator|.
-name|toExternalForm
-argument_list|()
-argument_list|,
-literal|true
-argument_list|)
-decl_stmt|;
-name|List
-argument_list|<
-name|String
-argument_list|>
-name|blacklist
-init|=
-operator|new
-name|ArrayList
-argument_list|<>
-argument_list|()
-decl_stmt|;
-name|blacklist
-operator|.
-name|add
+name|blacklistWith
 argument_list|(
 literal|"spring;range=\"[2,3)\""
 argument_list|)
-expr_stmt|;
-name|Blacklist
-operator|.
-name|blacklist
+decl_stmt|;
+name|assertTrue
 argument_list|(
 name|features
-argument_list|,
-name|blacklist
-argument_list|)
-expr_stmt|;
-for|for
-control|(
-name|Feature
-name|feature
-range|:
-name|features
 operator|.
-name|getFeature
-argument_list|()
-control|)
-block|{
-name|assertNotEquals
+name|noneMatch
 argument_list|(
-literal|"spring/2.5.6.SEC02"
-argument_list|,
-name|feature
+name|f
+lambda|->
+name|f
 operator|.
 name|getId
 argument_list|()
+operator|.
+name|equals
+argument_list|(
+literal|"spring/2.5.6.SEC02"
+argument_list|)
+argument_list|)
 argument_list|)
 expr_stmt|;
-block|}
 block|}
 annotation|@
 name|Test
@@ -250,81 +192,37 @@ name|void
 name|testBlacklistFeatureWithVersion
 parameter_list|()
 block|{
-name|URL
-name|url
-init|=
-name|getClass
-argument_list|()
-operator|.
-name|getResource
-argument_list|(
-literal|"f02.xml"
-argument_list|)
-decl_stmt|;
-name|Features
+name|Stream
+argument_list|<
+name|Feature
+argument_list|>
 name|features
 init|=
-name|JaxbUtil
-operator|.
-name|unmarshal
-argument_list|(
-name|url
-operator|.
-name|toExternalForm
-argument_list|()
-argument_list|,
-literal|true
-argument_list|)
-decl_stmt|;
-name|List
-argument_list|<
-name|String
-argument_list|>
-name|blacklist
-init|=
-operator|new
-name|ArrayList
-argument_list|<>
-argument_list|()
-decl_stmt|;
-name|blacklist
-operator|.
-name|add
+name|blacklistWith
 argument_list|(
 literal|"spring;range=2.5.6.SEC02"
 argument_list|)
-expr_stmt|;
-name|Blacklist
-operator|.
-name|blacklist
+decl_stmt|;
+name|assertTrue
 argument_list|(
 name|features
-argument_list|,
-name|blacklist
-argument_list|)
-expr_stmt|;
-for|for
-control|(
-name|Feature
-name|feature
-range|:
-name|features
 operator|.
-name|getFeature
-argument_list|()
-control|)
-block|{
-name|assertNotEquals
+name|noneMatch
 argument_list|(
-literal|"spring/2.5.6.SEC02"
-argument_list|,
-name|feature
+name|f
+lambda|->
+name|f
 operator|.
 name|getId
 argument_list|()
+operator|.
+name|equals
+argument_list|(
+literal|"spring/2.5.6.SEC02"
+argument_list|)
+argument_list|)
 argument_list|)
 expr_stmt|;
-block|}
 block|}
 annotation|@
 name|Test
@@ -333,73 +231,26 @@ name|void
 name|testBlacklistFeatureWithoutVersion
 parameter_list|()
 block|{
-name|URL
-name|url
-init|=
-name|getClass
-argument_list|()
-operator|.
-name|getResource
-argument_list|(
-literal|"f02.xml"
-argument_list|)
-decl_stmt|;
-name|Features
+name|Stream
+argument_list|<
+name|Feature
+argument_list|>
 name|features
 init|=
-name|JaxbUtil
-operator|.
-name|unmarshal
-argument_list|(
-name|url
-operator|.
-name|toExternalForm
-argument_list|()
-argument_list|,
-literal|true
-argument_list|)
-decl_stmt|;
-name|List
-argument_list|<
-name|String
-argument_list|>
-name|blacklist
-init|=
-operator|new
-name|ArrayList
-argument_list|<>
-argument_list|()
-decl_stmt|;
-name|blacklist
-operator|.
-name|add
+name|blacklistWith
 argument_list|(
 literal|"spring"
 argument_list|)
-expr_stmt|;
-name|Blacklist
-operator|.
-name|blacklist
+decl_stmt|;
+name|assertTrue
 argument_list|(
 name|features
-argument_list|,
-name|blacklist
-argument_list|)
-expr_stmt|;
-for|for
-control|(
-name|Feature
-name|feature
-range|:
-name|features
 operator|.
-name|getFeature
-argument_list|()
-control|)
-block|{
-name|assertFalse
+name|noneMatch
 argument_list|(
-name|feature
+name|f
+lambda|->
+name|f
 operator|.
 name|getId
 argument_list|()
@@ -409,8 +260,8 @@ argument_list|(
 literal|"spring/"
 argument_list|)
 argument_list|)
+argument_list|)
 expr_stmt|;
-block|}
 block|}
 annotation|@
 name|Test
@@ -418,6 +269,75 @@ specifier|public
 name|void
 name|testBlacklistBundle
 parameter_list|()
+block|{
+name|String
+name|blacklisted
+init|=
+literal|"mvn:org.apache.servicemix.bundles/org.apache.servicemix.bundles.jasypt/1.7_1"
+decl_stmt|;
+name|Stream
+argument_list|<
+name|Feature
+argument_list|>
+name|features
+init|=
+name|blacklistWith
+argument_list|(
+name|blacklisted
+argument_list|)
+decl_stmt|;
+name|Stream
+argument_list|<
+name|BundleInfo
+argument_list|>
+name|bundles
+init|=
+name|features
+operator|.
+name|flatMap
+argument_list|(
+name|f
+lambda|->
+name|f
+operator|.
+name|getBundles
+argument_list|()
+operator|.
+name|stream
+argument_list|()
+argument_list|)
+decl_stmt|;
+name|assertTrue
+argument_list|(
+name|bundles
+operator|.
+name|noneMatch
+argument_list|(
+name|b
+lambda|->
+name|b
+operator|.
+name|getLocation
+argument_list|()
+operator|.
+name|equals
+argument_list|(
+name|blacklisted
+argument_list|)
+argument_list|)
+argument_list|)
+expr_stmt|;
+block|}
+specifier|private
+name|Stream
+argument_list|<
+name|Feature
+argument_list|>
+name|blacklistWith
+parameter_list|(
+name|String
+name|blacklistClause
+parameter_list|)
 block|{
 name|URL
 name|url
@@ -445,67 +365,36 @@ argument_list|,
 literal|true
 argument_list|)
 decl_stmt|;
-name|List
-argument_list|<
-name|String
-argument_list|>
+name|Blacklist
 name|blacklist
 init|=
 operator|new
-name|ArrayList
-argument_list|<>
-argument_list|()
+name|Blacklist
+argument_list|(
+name|Collections
+operator|.
+name|singletonList
+argument_list|(
+name|blacklistClause
+argument_list|)
+argument_list|)
 decl_stmt|;
 name|blacklist
-operator|.
-name|add
-argument_list|(
-literal|"mvn:org.apache.servicemix.bundles/org.apache.servicemix.bundles.jasypt/1.7_1"
-argument_list|)
-expr_stmt|;
-name|Blacklist
 operator|.
 name|blacklist
 argument_list|(
 name|features
-argument_list|,
-name|blacklist
 argument_list|)
 expr_stmt|;
-for|for
-control|(
-name|Feature
-name|feature
-range|:
+return|return
 name|features
 operator|.
 name|getFeature
 argument_list|()
-control|)
-block|{
-for|for
-control|(
-name|Bundle
-name|bundle
-range|:
-name|feature
 operator|.
-name|getBundle
+name|stream
 argument_list|()
-control|)
-block|{
-name|assertNotEquals
-argument_list|(
-literal|"mvn:org.apache.servicemix.bundles/org.apache.servicemix.bundles.jasypt/1.7_1"
-argument_list|,
-name|bundle
-operator|.
-name|getLocation
-argument_list|()
-argument_list|)
-expr_stmt|;
-block|}
-block|}
+return|;
 block|}
 block|}
 end_class

features/core/src/main/java/org/apache/karaf/features/internal/service/FeaturesServiceImpl.java
@@ -369,22 +369,6 @@ name|felix
 operator|.
 name|utils
 operator|.
-name|manifest
-operator|.
-name|Clause
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|felix
-operator|.
-name|utils
-operator|.
 name|version
 operator|.
 name|VersionCleaner
@@ -1204,6 +1188,17 @@ name|globalRepository
 operator|=
 name|globalRepository
 expr_stmt|;
+name|Blacklist
+name|blacklist
+init|=
+operator|new
+name|Blacklist
+argument_list|(
+name|cfg
+operator|.
+name|blacklisted
+argument_list|)
+decl_stmt|;
 name|this
 operator|.
 name|repositories
@@ -1211,9 +1206,7 @@ operator|=
 operator|new
 name|RepositoryCache
 argument_list|(
-name|cfg
-operator|.
-name|blacklisted
+name|blacklist
 argument_list|)
 expr_stmt|;
 name|this
@@ -1829,6 +1822,17 @@ argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
+name|Blacklist
+name|blacklist
+init|=
+operator|new
+name|Blacklist
+argument_list|(
+name|cfg
+operator|.
+name|blacklisted
+argument_list|)
+decl_stmt|;
 for|for
 control|(
 name|String
@@ -1850,9 +1854,7 @@ argument_list|(
 name|uri
 argument_list|)
 argument_list|,
-name|cfg
-operator|.
-name|blacklisted
+name|blacklist
 argument_list|)
 decl_stmt|;
 name|listener
@@ -6286,155 +6288,6 @@ literal|true
 argument_list|,
 literal|true
 argument_list|)
-return|;
-block|}
-specifier|private
-name|Map
-argument_list|<
-name|String
-argument_list|,
-name|Feature
-argument_list|>
-name|loadAllFeatures
-parameter_list|(
-name|Set
-argument_list|<
-name|URI
-argument_list|>
-name|uris
-parameter_list|)
-throws|throws
-name|Exception
-block|{
-comment|//the outer map's key is feature name, the inner map's key is feature version
-name|Map
-argument_list|<
-name|String
-argument_list|,
-name|Feature
-argument_list|>
-name|map
-init|=
-operator|new
-name|HashMap
-argument_list|<>
-argument_list|()
-decl_stmt|;
-comment|// Two phase load:
-comment|// * first load dependent repositories
-name|Set
-argument_list|<
-name|URI
-argument_list|>
-name|loaded
-init|=
-operator|new
-name|HashSet
-argument_list|<>
-argument_list|()
-decl_stmt|;
-name|List
-argument_list|<
-name|URI
-argument_list|>
-name|toLoad
-init|=
-operator|new
-name|ArrayList
-argument_list|<>
-argument_list|(
-name|uris
-argument_list|)
-decl_stmt|;
-name|Clause
-index|[]
-name|blacklisted
-init|=
-name|repositories
-operator|.
-name|getBlacklisted
-argument_list|()
-decl_stmt|;
-while|while
-condition|(
-operator|!
-name|toLoad
-operator|.
-name|isEmpty
-argument_list|()
-condition|)
-block|{
-name|URI
-name|uri
-init|=
-name|toLoad
-operator|.
-name|remove
-argument_list|(
-literal|0
-argument_list|)
-decl_stmt|;
-if|if
-condition|(
-name|loaded
-operator|.
-name|add
-argument_list|(
-name|uri
-argument_list|)
-condition|)
-block|{
-name|Repository
-name|repo
-init|=
-operator|new
-name|RepositoryImpl
-argument_list|(
-name|uri
-argument_list|,
-name|blacklisted
-argument_list|)
-decl_stmt|;
-name|Collections
-operator|.
-name|addAll
-argument_list|(
-name|toLoad
-argument_list|,
-name|repo
-operator|.
-name|getRepositories
-argument_list|()
-argument_list|)
-expr_stmt|;
-for|for
-control|(
-name|Feature
-name|f
-range|:
-name|repo
-operator|.
-name|getFeatures
-argument_list|()
-control|)
-block|{
-name|map
-operator|.
-name|put
-argument_list|(
-name|f
-operator|.
-name|getId
-argument_list|()
-argument_list|,
-name|f
-argument_list|)
-expr_stmt|;
-block|}
-block|}
-block|}
-return|return
-name|map
 return|;
 block|}
 annotation|@

features/core/src/main/java/org/apache/karaf/features/internal/service/RepositoryCache.java
@@ -125,38 +125,6 @@ name|org
 operator|.
 name|apache
 operator|.
-name|felix
-operator|.
-name|utils
-operator|.
-name|manifest
-operator|.
-name|Clause
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|felix
-operator|.
-name|utils
-operator|.
-name|manifest
-operator|.
-name|Parser
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
 name|karaf
 operator|.
 name|features
@@ -187,80 +155,22 @@ argument_list|()
 decl_stmt|;
 specifier|private
 specifier|final
-name|Clause
-index|[]
-name|blacklisted
+name|Blacklist
+name|blacklist
 decl_stmt|;
 specifier|public
 name|RepositoryCache
 parameter_list|(
-name|String
-name|blacklisted
+name|Blacklist
+name|blacklist
 parameter_list|)
 block|{
 name|this
 operator|.
-name|blacklisted
+name|blacklist
 operator|=
-name|loadBlacklist
-argument_list|(
-name|blacklisted
-argument_list|)
+name|blacklist
 expr_stmt|;
-block|}
-specifier|public
-name|Clause
-index|[]
-name|getBlacklisted
-parameter_list|()
-block|{
-return|return
-name|blacklisted
-return|;
-block|}
-specifier|private
-specifier|static
-name|Clause
-index|[]
-name|loadBlacklist
-parameter_list|(
-name|String
-name|blacklisted
-parameter_list|)
-block|{
-name|Set
-argument_list|<
-name|String
-argument_list|>
-name|blacklistStrings
-init|=
-name|Blacklist
-operator|.
-name|loadBlacklist
-argument_list|(
-name|blacklisted
-argument_list|)
-decl_stmt|;
-return|return
-name|Parser
-operator|.
-name|parseClauses
-argument_list|(
-name|blacklistStrings
-operator|.
-name|toArray
-argument_list|(
-operator|new
-name|String
-index|[
-name|blacklistStrings
-operator|.
-name|size
-argument_list|()
-index|]
-argument_list|)
-argument_list|)
-return|;
 block|}
 specifier|public
 name|Repository
@@ -286,7 +196,7 @@ name|RepositoryImpl
 argument_list|(
 name|uri
 argument_list|,
-name|blacklisted
+name|blacklist
 argument_list|)
 decl_stmt|;
 if|if

features/core/src/main/java/org/apache/karaf/features/internal/service/RepositoryImpl.java
@@ -81,48 +81,6 @@ end_import
 
 begin_import
 import|import
-name|java
-operator|.
-name|util
-operator|.
-name|Set
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|felix
-operator|.
-name|utils
-operator|.
-name|manifest
-operator|.
-name|Clause
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|felix
-operator|.
-name|utils
-operator|.
-name|manifest
-operator|.
-name|Parser
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
@@ -203,9 +161,8 @@ name|uri
 decl_stmt|;
 specifier|private
 specifier|final
-name|Clause
-index|[]
-name|blacklisted
+name|Blacklist
+name|blacklist
 decl_stmt|;
 specifier|private
 name|Features
@@ -222,10 +179,6 @@ name|this
 argument_list|(
 name|uri
 argument_list|,
-operator|(
-name|Clause
-index|[]
-operator|)
 literal|null
 argument_list|)
 expr_stmt|;
@@ -236,62 +189,8 @@ parameter_list|(
 name|URI
 name|uri
 parameter_list|,
-name|String
-name|blacklisted
-parameter_list|)
-block|{
-name|this
-operator|.
-name|uri
-operator|=
-name|uri
-expr_stmt|;
-name|Set
-argument_list|<
-name|String
-argument_list|>
-name|blacklistStrings
-init|=
 name|Blacklist
-operator|.
-name|loadBlacklist
-argument_list|(
-name|blacklisted
-argument_list|)
-decl_stmt|;
-name|this
-operator|.
-name|blacklisted
-operator|=
-name|Parser
-operator|.
-name|parseClauses
-argument_list|(
-name|blacklistStrings
-operator|.
-name|toArray
-argument_list|(
-operator|new
-name|String
-index|[
-name|blacklistStrings
-operator|.
-name|size
-argument_list|()
-index|]
-argument_list|)
-argument_list|)
-expr_stmt|;
-block|}
-specifier|public
-name|RepositoryImpl
-parameter_list|(
-name|URI
-name|uri
-parameter_list|,
-name|Clause
-index|[]
-name|blacklisted
+name|blacklist
 parameter_list|)
 block|{
 name|this
@@ -302,19 +201,9 @@ name|uri
 expr_stmt|;
 name|this
 operator|.
-name|blacklisted
+name|blacklist
 operator|=
-name|blacklisted
-operator|!=
-literal|null
-condition|?
-name|blacklisted
-else|:
-operator|new
-name|Clause
-index|[
-literal|0
-index|]
+name|blacklist
 expr_stmt|;
 block|}
 specifier|public
@@ -526,15 +415,21 @@ argument_list|,
 name|validate
 argument_list|)
 expr_stmt|;
-name|Blacklist
+if|if
+condition|(
+name|blacklist
+operator|!=
+literal|null
+condition|)
+block|{
+name|blacklist
 operator|.
 name|blacklist
 argument_list|(
 name|features
-argument_list|,
-name|blacklisted
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 catch|catch
 parameter_list|(

commit 95d0a807c4d1bd8d00bd996ed14582a615034b76

Fix blacklist mechanism broken by commit bc72970be35e0f62ceb4341554189b00e6bcbeea [formerly eb95bce7d5a1bcf23a5bda919209394ffcf3c01a]
Former-commit-id: 1103f3de2c37539f7e2ee695c5bfe873ef47abc2

features/core/src/main/java/org/apache/karaf/features/internal/service/Blacklist.java
@@ -473,12 +473,9 @@ comment|//
 operator|.
 name|map
 argument_list|(
-name|line
-lambda|->
-name|line
-operator|.
+name|String
+operator|::
 name|trim
-argument_list|()
 argument_list|)
 comment|//
 operator|.
@@ -486,11 +483,13 @@ name|filter
 argument_list|(
 name|line
 lambda|->
+operator|!
 name|line
 operator|.
 name|isEmpty
 argument_list|()
-operator|||
+operator|&&
+operator|!
 name|line
 operator|.
 name|startsWith
@@ -499,10 +498,11 @@ literal|"#"
 argument_list|)
 argument_list|)
 operator|.
-name|collect
+name|forEach
 argument_list|(
-name|toSet
-argument_list|()
+name|blacklist
+operator|::
+name|add
 argument_list|)
 expr_stmt|;
 block|}
@@ -586,12 +586,9 @@ argument_list|()
 operator|.
 name|removeIf
 argument_list|(
-name|feature
-lambda|->
+name|this
+operator|::
 name|blacklist
-argument_list|(
-name|feature
-argument_list|)
 argument_list|)
 expr_stmt|;
 block|}

features/core/src/test/java/org/apache/karaf/features/internal/service/BlacklistTest.java
@@ -308,6 +308,95 @@ argument_list|)
 argument_list|)
 expr_stmt|;
 block|}
+annotation|@
+name|Test
+specifier|public
+name|void
+name|testBlacklistLoad
+parameter_list|()
+throws|throws
+name|URISyntaxException
+block|{
+name|Blacklist
+name|blacklist
+init|=
+operator|new
+name|Blacklist
+argument_list|(
+name|getClass
+argument_list|()
+operator|.
+name|getResource
+argument_list|(
+literal|"blacklist.txt"
+argument_list|)
+operator|.
+name|toExternalForm
+argument_list|()
+argument_list|)
+decl_stmt|;
+name|RepositoryImpl
+name|repo
+init|=
+operator|new
+name|RepositoryImpl
+argument_list|(
+name|getClass
+argument_list|()
+operator|.
+name|getResource
+argument_list|(
+literal|"f02.xml"
+argument_list|)
+operator|.
+name|toURI
+argument_list|()
+argument_list|,
+name|blacklist
+argument_list|,
+literal|true
+argument_list|)
+decl_stmt|;
+name|Stream
+argument_list|<
+name|Feature
+argument_list|>
+name|features
+init|=
+name|Arrays
+operator|.
+name|asList
+argument_list|(
+name|repo
+operator|.
+name|getFeatures
+argument_list|()
+argument_list|)
+operator|.
+name|stream
+argument_list|()
+decl_stmt|;
+name|assertTrue
+argument_list|(
+name|features
+operator|.
+name|noneMatch
+argument_list|(
+name|f
+lambda|->
+name|f
+operator|.
+name|getId
+argument_list|()
+operator|.
+name|equals
+argument_list|(
+literal|"spring/2.5.6.SEC02"
+argument_list|)
+argument_list|)
+argument_list|)
+expr_stmt|;
+block|}
 specifier|private
 name|Stream
 argument_list|<