sho25/karaf

commit c9dc4bf7d04e18d993d984a1f01bda1a36f564fb

[KARAF-5178] Use lambdas where appropriate
Signed-off-by: Stephen Kitt <skitt@redhat.com>

Former-commit-id: bf666631fe84f0394a9b948b5088d205bf52db36

config/src/main/java/org/apache/karaf/config/command/completers/MetaCompleter.java
@@ -75,24 +75,6 @@ name|apache
 operator|.
 name|karaf
 operator|.
-name|config
-operator|.
-name|core
-operator|.
-name|impl
-operator|.
-name|MetatypeCallable
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|karaf
-operator|.
 name|shell
 operator|.
 name|api
@@ -279,20 +261,6 @@ name|MetaTypeInformation
 import|;
 end_import
 
-begin_import
-import|import
-name|org
-operator|.
-name|osgi
-operator|.
-name|service
-operator|.
-name|metatype
-operator|.
-name|MetaTypeService
-import|;
-end_import
-
 begin_class
 annotation|@
 name|Service
@@ -410,34 +378,14 @@ name|withMetaTypeService
 argument_list|(
 name|context
 argument_list|,
-operator|new
-name|MetatypeCallable
-argument_list|<
-name|List
-argument_list|<
-name|String
-argument_list|>
-argument_list|>
-argument_list|()
-block|{
-annotation|@
-name|Override
-specifier|public
-name|List
-argument_list|<
-name|String
-argument_list|>
-name|callWith
-parameter_list|(
-name|MetaTypeService
 name|metatypeService
-parameter_list|)
+lambda|->
 block|{
 name|List
 argument_list|<
 name|String
 argument_list|>
-name|pids
+name|pids1
 init|=
 operator|new
 name|ArrayList
@@ -490,7 +438,7 @@ operator|!=
 literal|null
 condition|)
 block|{
-name|pids
+name|pids1
 operator|.
 name|addAll
 argument_list|(
@@ -516,7 +464,7 @@ operator|!=
 literal|null
 condition|)
 block|{
-name|pids
+name|pids1
 operator|.
 name|addAll
 argument_list|(
@@ -534,9 +482,8 @@ expr_stmt|;
 block|}
 block|}
 return|return
-name|pids
+name|pids1
 return|;
-block|}
 block|}
 argument_list|)
 decl_stmt|;

bundle/core/src/main/java/org/apache/karaf/bundle/command/Headers.java
@@ -1881,40 +1881,14 @@ name|sort
 argument_list|(
 name|directives
 argument_list|,
-operator|new
 name|Comparator
-argument_list|<
-name|Directive
-argument_list|>
-argument_list|()
-block|{
-specifier|public
-name|int
-name|compare
-parameter_list|(
-name|Directive
-name|o1
-parameter_list|,
-name|Directive
-name|o2
-parameter_list|)
-block|{
-return|return
-name|o1
 operator|.
-name|getName
-argument_list|()
-operator|.
-name|compareTo
+name|comparing
 argument_list|(
-name|o2
-operator|.
+name|Directive
+operator|::
 name|getName
-argument_list|()
 argument_list|)
-return|;
-block|}
-block|}
 argument_list|)
 expr_stmt|;
 name|Arrays
@@ -1923,40 +1897,14 @@ name|sort
 argument_list|(
 name|attributes
 argument_list|,
-operator|new
 name|Comparator
-argument_list|<
-name|Attribute
-argument_list|>
-argument_list|()
-block|{
-specifier|public
-name|int
-name|compare
-parameter_list|(
-name|Attribute
-name|o1
-parameter_list|,
-name|Attribute
-name|o2
-parameter_list|)
-block|{
-return|return
-name|o1
 operator|.
-name|getName
-argument_list|()
-operator|.
-name|compareTo
+name|comparing
 argument_list|(
-name|o2
-operator|.
+name|Attribute
+operator|::
 name|getName
-argument_list|()
 argument_list|)
-return|;
-block|}
-block|}
 argument_list|)
 expr_stmt|;
 name|builder

bundle/core/src/main/java/org/apache/karaf/bundle/command/LoadTest.java
@@ -249,18 +249,6 @@ name|osgi
 operator|.
 name|framework
 operator|.
-name|FrameworkEvent
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|osgi
-operator|.
-name|framework
-operator|.
 name|FrameworkListener
 import|;
 end_import
@@ -606,12 +594,9 @@ control|)
 block|{
 operator|new
 name|Thread
-argument_list|()
-block|{
-specifier|public
-name|void
-name|run
+argument_list|(
 parameter_list|()
+lambda|->
 block|{
 try|try
 block|{
@@ -714,7 +699,7 @@ argument_list|()
 expr_stmt|;
 specifier|final
 name|CountDownLatch
-name|latch
+name|latch1
 init|=
 operator|new
 name|CountDownLatch
@@ -736,28 +721,18 @@ name|b
 index|]
 argument_list|)
 argument_list|,
-operator|new
+operator|(
 name|FrameworkListener
-argument_list|()
-block|{
-specifier|public
-name|void
-name|frameworkEvent
-parameter_list|(
-name|FrameworkEvent
+operator|)
 name|event
-parameter_list|)
-block|{
-name|latch
+lambda|->
+name|latch1
 operator|.
 name|countDown
 argument_list|()
-expr_stmt|;
-block|}
-block|}
 argument_list|)
 expr_stmt|;
-name|latch
+name|latch1
 operator|.
 name|await
 argument_list|()
@@ -988,7 +963,7 @@ argument_list|()
 expr_stmt|;
 block|}
 block|}
-block|}
+argument_list|)
 operator|.
 name|start
 argument_list|()
@@ -996,14 +971,9 @@ expr_stmt|;
 block|}
 operator|new
 name|Thread
-argument_list|()
-block|{
-annotation|@
-name|Override
-specifier|public
-name|void
-name|run
+argument_list|(
 parameter_list|()
+lambda|->
 block|{
 try|try
 block|{
@@ -1035,7 +1005,7 @@ literal|"Load test finished"
 argument_list|)
 expr_stmt|;
 block|}
-block|}
+argument_list|)
 operator|.
 name|start
 argument_list|()

bundle/core/src/main/java/org/apache/karaf/bundle/command/ShowBundleTree.java
@@ -635,25 +635,8 @@ name|System
 operator|.
 name|out
 argument_list|,
-operator|new
-name|Tree
-operator|.
-name|Converter
-argument_list|<
-name|Bundle
-argument_list|>
-argument_list|()
-block|{
-specifier|public
-name|String
-name|toString
-parameter_list|(
-name|Node
-argument_list|<
-name|Bundle
-argument_list|>
 name|node
-parameter_list|)
+lambda|->
 block|{
 if|if
 condition|(
@@ -722,7 +705,6 @@ name|getBundleId
 argument_list|()
 argument_list|)
 return|;
-block|}
 block|}
 block|}
 argument_list|)

bundle/core/src/main/java/org/apache/karaf/bundle/command/bundletree/Tree.java
@@ -129,25 +129,8 @@ name|write
 argument_list|(
 name|writer
 argument_list|,
-operator|new
-name|Converter
-argument_list|<
-name|T
-argument_list|>
-argument_list|()
-block|{
-specifier|public
-name|String
-name|toString
-parameter_list|(
-name|Node
-argument_list|<
-name|T
-argument_list|>
 name|node
-parameter_list|)
-block|{
-return|return
+lambda|->
 name|node
 operator|.
 name|getValue
@@ -155,9 +138,6 @@ argument_list|()
 operator|.
 name|toString
 argument_list|()
-return|;
-block|}
-block|}
 argument_list|)
 expr_stmt|;
 block|}

bundle/core/src/main/java/org/apache/karaf/bundle/core/internal/BundleWatcherImpl.java
@@ -690,25 +690,15 @@ name|refreshBundles
 argument_list|(
 name|updated
 argument_list|,
-operator|new
+operator|(
 name|FrameworkListener
-argument_list|()
-block|{
-specifier|public
-name|void
-name|frameworkEvent
-parameter_list|(
-name|FrameworkEvent
+operator|)
 name|event
-parameter_list|)
-block|{
+lambda|->
 name|latch
 operator|.
 name|countDown
 argument_list|()
-expr_stmt|;
-block|}
-block|}
 argument_list|)
 expr_stmt|;
 name|latch

bundle/core/src/test/java/org/apache/karaf/bundle/command/bundletree/TreeTest.java
@@ -131,42 +131,6 @@ begin_import
 import|import
 name|org
 operator|.
-name|apache
-operator|.
-name|karaf
-operator|.
-name|bundle
-operator|.
-name|command
-operator|.
-name|bundletree
-operator|.
-name|Node
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|karaf
-operator|.
-name|bundle
-operator|.
-name|command
-operator|.
-name|bundletree
-operator|.
-name|Tree
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
 name|junit
 operator|.
 name|Test
@@ -174,7 +138,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Test cases for {@link org.apache.karaf.shell.dev.util.Tree}  * and {@link org.apache.karaf.shell.dev.util.Node}  */
+comment|/**  * Test cases for {@link org.apache.karaf.bundle.command.bundletree.Tree}  * and {@link org.apache.karaf.bundle.command.bundletree.Node}  */
 end_comment
 
 begin_class
@@ -286,36 +250,14 @@ argument_list|(
 name|writer
 argument_list|)
 argument_list|,
-operator|new
-name|Tree
-operator|.
-name|Converter
-argument_list|<
-name|String
-argument_list|>
-argument_list|()
-block|{
-specifier|public
-name|String
-name|toString
-parameter_list|(
-name|Node
-argument_list|<
-name|String
-argument_list|>
 name|node
-parameter_list|)
-block|{
-return|return
+lambda|->
 literal|"my "
 operator|+
 name|node
 operator|.
 name|getValue
 argument_list|()
-return|;
-block|}
-block|}
 argument_list|)
 expr_stmt|;
 name|BufferedReader

client/src/main/java/org/apache/karaf/client/Main.java
@@ -133,30 +133,6 @@ name|lang
 operator|.
 name|reflect
 operator|.
-name|InvocationHandler
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|lang
-operator|.
-name|reflect
-operator|.
-name|Method
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|lang
-operator|.
-name|reflect
-operator|.
 name|Proxy
 import|;
 end_import
@@ -922,21 +898,8 @@ condition|)
 block|{
 name|passwordProvider
 operator|=
-operator|new
-name|FilePasswordProvider
-argument_list|()
-block|{
-annotation|@
-name|Override
-specifier|public
-name|String
-name|getPassword
-parameter_list|(
-name|String
 name|resourceKey
-parameter_list|)
-throws|throws
-name|IOException
+lambda|->
 block|{
 name|char
 index|[]
@@ -960,7 +923,6 @@ argument_list|(
 name|pwd
 argument_list|)
 return|;
-block|}
 block|}
 expr_stmt|;
 name|client
@@ -2930,26 +2892,14 @@ block|{
 name|signalHandlerClass
 block|}
 operator|,
-operator|new
-name|InvocationHandler
-argument_list|()
-block|{
-specifier|public
-name|Object
-name|invoke
 parameter_list|(
-name|Object
 name|proxy
 parameter_list|,
-name|Method
 name|method
 parameter_list|,
-name|Object
-index|[]
 name|args
 parameter_list|)
-throws|throws
-name|Throwable
+lambda|->
 block|{
 name|Size
 name|size
@@ -2977,7 +2927,6 @@ expr_stmt|;
 return|return
 literal|null
 return|;
-block|}
 block|}
 block|)
 empty_stmt|;

commit 5c91d1eb3fea1dd01578580cc61666be95773257

Fix meta command / completer to not print a warning when MetaType is not available
I think this is a regression caused by https://github.com/apache/karaf/commit/c9dc4bf7d04e18d993d984a1f01bda1a36f564fb [formerly bf666631fe84f0394a9b948b5088d205bf52db36]#diff-290cb1e2332aeecaf61aaa6aa3602768
which removes the anonymous inner classes and thus cause a CNFE to be thrown when the MetaCompleter getMethods() is called for introspection.
So this commit slightly change the mechanism and a warning is displayed when the completer is initialized, while a message is printed on the console when calling the command.


Former-commit-id: 475be9310ebdc168b73d7b6a49763aab51698084

config/src/main/java/org/apache/karaf/config/command/MetaCommand.java
@@ -89,19 +89,13 @@ end_import
 
 begin_import
 import|import
-name|org
+name|java
 operator|.
-name|apache
+name|util
 operator|.
-name|karaf
+name|function
 operator|.
-name|config
-operator|.
-name|command
-operator|.
-name|completers
-operator|.
-name|MetaCompleter
+name|Function
 import|;
 end_import
 
@@ -115,11 +109,11 @@ name|karaf
 operator|.
 name|config
 operator|.
-name|core
+name|command
 operator|.
-name|impl
+name|completers
 operator|.
-name|MetatypeCallable
+name|MetaCompleter
 import|;
 end_import
 
@@ -247,6 +241,22 @@ name|shell
 operator|.
 name|support
 operator|.
+name|CommandException
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|karaf
+operator|.
+name|shell
+operator|.
+name|support
+operator|.
 name|table
 operator|.
 name|ShellTable
@@ -347,6 +357,26 @@ name|ObjectClassDefinition
 import|;
 end_import
 
+begin_import
+import|import
+name|org
+operator|.
+name|slf4j
+operator|.
+name|Logger
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|slf4j
+operator|.
+name|LoggerFactory
+import|;
+end_import
+
 begin_class
 annotation|@
 name|Command
@@ -371,6 +401,21 @@ name|MetaCommand
 extends|extends
 name|ConfigCommandSupport
 block|{
+specifier|private
+specifier|static
+specifier|final
+name|Logger
+name|LOG
+init|=
+name|LoggerFactory
+operator|.
+name|getLogger
+argument_list|(
+name|MetaCommand
+operator|.
+name|class
+argument_list|)
+decl_stmt|;
 annotation|@
 name|Argument
 argument_list|(
@@ -569,6 +614,8 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
+try|try
+block|{
 if|if
 condition|(
 name|create
@@ -600,12 +647,82 @@ return|return
 literal|null
 return|;
 block|}
+catch|catch
+parameter_list|(
+name|Throwable
+name|e
+parameter_list|)
+block|{
+name|Throwable
+name|ncdfe
+init|=
+name|e
+decl_stmt|;
+while|while
+condition|(
+name|ncdfe
+operator|!=
+literal|null
+operator|&&
+operator|!
+operator|(
+name|ncdfe
+operator|instanceof
+name|NoClassDefFoundError
+operator|)
+condition|)
+block|{
+name|ncdfe
+operator|=
+name|ncdfe
+operator|.
+name|getCause
+argument_list|()
+expr_stmt|;
+block|}
+if|if
+condition|(
+name|ncdfe
+operator|!=
+literal|null
+operator|&&
+name|ncdfe
+operator|.
+name|getMessage
+argument_list|()
+operator|.
+name|equals
+argument_list|(
+literal|"org/osgi/service/metatype/MetaTypeService"
+argument_list|)
+condition|)
+block|{
+throw|throw
+operator|new
+name|CommandException
+argument_list|(
+literal|"config:meta disabled because the org.osgi.service.metatype package is not wired"
+argument_list|,
+name|e
+argument_list|)
+throw|;
+block|}
+else|else
+block|{
+throw|throw
+name|e
+throw|;
+block|}
+block|}
+block|}
 specifier|abstract
 class|class
 name|AbstractMeta
 implements|implements
-name|MetatypeCallable
+name|Function
 argument_list|<
+name|MetaTypeService
+argument_list|,
 name|Void
 argument_list|>
 block|{
@@ -777,7 +894,7 @@ name|AbstractMeta
 block|{
 specifier|public
 name|Void
-name|callWith
+name|apply
 parameter_list|(
 name|MetaTypeService
 name|metaTypeService
@@ -965,7 +1082,7 @@ name|AbstractMeta
 block|{
 specifier|public
 name|Void
-name|callWith
+name|apply
 parameter_list|(
 name|MetaTypeService
 name|metaTypeService

config/src/main/java/org/apache/karaf/config/command/completers/MetaCompleter.java
@@ -25,26 +25,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|ArrayList
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
-name|Arrays
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|List
 import|;
 end_import
@@ -207,18 +187,6 @@ name|osgi
 operator|.
 name|framework
 operator|.
-name|Bundle
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|osgi
-operator|.
-name|framework
-operator|.
 name|BundleContext
 import|;
 end_import
@@ -251,13 +219,19 @@ begin_import
 import|import
 name|org
 operator|.
-name|osgi
+name|slf4j
 operator|.
-name|service
+name|Logger
+import|;
+end_import
+
+begin_import
+import|import
+name|org
 operator|.
-name|metatype
+name|slf4j
 operator|.
-name|MetaTypeInformation
+name|LoggerFactory
 import|;
 end_import
 
@@ -272,6 +246,21 @@ name|Completer
 implements|,
 name|BundleListener
 block|{
+specifier|private
+specifier|static
+specifier|final
+name|Logger
+name|LOG
+init|=
+name|LoggerFactory
+operator|.
+name|getLogger
+argument_list|(
+name|MetaCompleter
+operator|.
+name|class
+argument_list|)
+decl_stmt|;
 specifier|private
 specifier|final
 name|StringsCompleter
@@ -293,6 +282,11 @@ name|void
 name|init
 parameter_list|()
 block|{
+try|try
+block|{
+name|updateMeta
+argument_list|()
+expr_stmt|;
 name|context
 operator|.
 name|registerService
@@ -306,9 +300,93 @@ argument_list|,
 literal|null
 argument_list|)
 expr_stmt|;
-name|updateMeta
+block|}
+catch|catch
+parameter_list|(
+name|Throwable
+name|e
+parameter_list|)
+block|{
+name|Throwable
+name|ncdfe
+init|=
+name|e
+decl_stmt|;
+while|while
+condition|(
+name|ncdfe
+operator|!=
+literal|null
+operator|&&
+operator|!
+operator|(
+name|ncdfe
+operator|instanceof
+name|NoClassDefFoundError
+operator|)
+condition|)
+block|{
+name|ncdfe
+operator|=
+name|ncdfe
+operator|.
+name|getCause
 argument_list|()
 expr_stmt|;
+block|}
+if|if
+condition|(
+name|ncdfe
+operator|!=
+literal|null
+operator|&&
+name|ncdfe
+operator|.
+name|getMessage
+argument_list|()
+operator|.
+name|equals
+argument_list|(
+literal|"org/osgi/service/metatype/MetaTypeService"
+argument_list|)
+condition|)
+block|{
+if|if
+condition|(
+name|LOG
+operator|.
+name|isDebugEnabled
+argument_list|()
+condition|)
+block|{
+name|LOG
+operator|.
+name|warn
+argument_list|(
+literal|"config:meta disabled because the org.osgi.service.metatype package is not wired"
+argument_list|,
+name|e
+argument_list|)
+expr_stmt|;
+block|}
+else|else
+block|{
+name|LOG
+operator|.
+name|warn
+argument_list|(
+literal|"config:meta disabled because the org.osgi.service.metatype package is not wired (enable debug logging for full stack trace)."
+argument_list|)
+expr_stmt|;
+block|}
+block|}
+else|else
+block|{
+throw|throw
+name|e
+throw|;
+block|}
+block|}
 block|}
 annotation|@
 name|Override
@@ -374,117 +452,9 @@ name|pids
 init|=
 name|MetaServiceCaller
 operator|.
-name|withMetaTypeService
+name|getPidsWithMetaInfo
 argument_list|(
 name|context
-argument_list|,
-name|metatypeService
-lambda|->
-block|{
-name|List
-argument_list|<
-name|String
-argument_list|>
-name|pids1
-init|=
-operator|new
-name|ArrayList
-argument_list|<>
-argument_list|()
-decl_stmt|;
-name|Bundle
-index|[]
-name|bundles
-init|=
-name|context
-operator|.
-name|getBundles
-argument_list|()
-decl_stmt|;
-for|for
-control|(
-name|Bundle
-name|bundle
-range|:
-name|bundles
-control|)
-block|{
-name|MetaTypeInformation
-name|info
-init|=
-name|metatypeService
-operator|.
-name|getMetaTypeInformation
-argument_list|(
-name|bundle
-argument_list|)
-decl_stmt|;
-if|if
-condition|(
-name|info
-operator|==
-literal|null
-condition|)
-block|{
-continue|continue;
-block|}
-if|if
-condition|(
-name|info
-operator|.
-name|getFactoryPids
-argument_list|()
-operator|!=
-literal|null
-condition|)
-block|{
-name|pids1
-operator|.
-name|addAll
-argument_list|(
-name|Arrays
-operator|.
-name|asList
-argument_list|(
-name|info
-operator|.
-name|getFactoryPids
-argument_list|()
-argument_list|)
-argument_list|)
-expr_stmt|;
-block|}
-if|if
-condition|(
-name|info
-operator|.
-name|getPids
-argument_list|()
-operator|!=
-literal|null
-condition|)
-block|{
-name|pids1
-operator|.
-name|addAll
-argument_list|(
-name|Arrays
-operator|.
-name|asList
-argument_list|(
-name|info
-operator|.
-name|getPids
-argument_list|()
-argument_list|)
-argument_list|)
-expr_stmt|;
-block|}
-block|}
-return|return
-name|pids1
-return|;
-block|}
 argument_list|)
 decl_stmt|;
 if|if

config/src/main/java/org/apache/karaf/config/core/impl/MetaServiceCaller.java
@@ -27,6 +27,18 @@ name|osgi
 operator|.
 name|framework
 operator|.
+name|Bundle
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|osgi
+operator|.
+name|framework
+operator|.
 name|BundleContext
 import|;
 end_import
@@ -40,6 +52,20 @@ operator|.
 name|framework
 operator|.
 name|ServiceReference
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|osgi
+operator|.
+name|service
+operator|.
+name|metatype
+operator|.
+name|MetaTypeInformation
 import|;
 end_import
 
@@ -77,6 +103,48 @@ name|LoggerFactory
 import|;
 end_import
 
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|ArrayList
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Arrays
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|List
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|function
+operator|.
+name|Function
+import|;
+end_import
+
 begin_comment
 comment|/**  * Allows to use the MetaTypeService as an optional dependency  */
 end_comment
@@ -86,20 +154,6 @@ specifier|public
 class|class
 name|MetaServiceCaller
 block|{
-specifier|private
-specifier|static
-name|Logger
-name|LOG
-init|=
-name|LoggerFactory
-operator|.
-name|getLogger
-argument_list|(
-name|MetaServiceCaller
-operator|.
-name|class
-argument_list|)
-decl_stmt|;
 specifier|public
 specifier|static
 parameter_list|<
@@ -111,14 +165,14 @@ parameter_list|(
 name|BundleContext
 name|context
 parameter_list|,
-name|MetatypeCallable
+name|Function
 argument_list|<
+name|MetaTypeService
+argument_list|,
 name|T
 argument_list|>
 name|callable
 parameter_list|)
-block|{
-try|try
 block|{
 name|ServiceReference
 argument_list|<
@@ -150,7 +204,7 @@ decl_stmt|;
 return|return
 name|callable
 operator|.
-name|callWith
+name|apply
 argument_list|(
 name|metaService
 argument_list|)
@@ -167,23 +221,132 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
-catch|catch
+specifier|public
+specifier|static
+name|List
+argument_list|<
+name|String
+argument_list|>
+name|getPidsWithMetaInfo
 parameter_list|(
-name|NoClassDefFoundError
-name|e
+name|BundleContext
+name|context
 parameter_list|)
 block|{
-name|LOG
-operator|.
-name|warn
+return|return
+name|withMetaTypeService
 argument_list|(
-literal|"No Metatype Service present"
+name|context
+argument_list|,
+name|metatypeService
+lambda|->
+block|{
+name|List
+argument_list|<
+name|String
+argument_list|>
+name|pids1
+init|=
+operator|new
+name|ArrayList
+argument_list|<>
+argument_list|()
+decl_stmt|;
+name|Bundle
+index|[]
+name|bundles
+init|=
+name|context
+operator|.
+name|getBundles
+argument_list|()
+decl_stmt|;
+for|for
+control|(
+name|Bundle
+name|bundle
+range|:
+name|bundles
+control|)
+block|{
+name|MetaTypeInformation
+name|info
+init|=
+name|metatypeService
+operator|.
+name|getMetaTypeInformation
+argument_list|(
+name|bundle
+argument_list|)
+decl_stmt|;
+if|if
+condition|(
+name|info
+operator|==
+literal|null
+condition|)
+block|{
+continue|continue;
+block|}
+if|if
+condition|(
+name|info
+operator|.
+name|getFactoryPids
+argument_list|()
+operator|!=
+literal|null
+condition|)
+block|{
+name|pids1
+operator|.
+name|addAll
+argument_list|(
+name|Arrays
+operator|.
+name|asList
+argument_list|(
+name|info
+operator|.
+name|getFactoryPids
+argument_list|()
+argument_list|)
 argument_list|)
 expr_stmt|;
-return|return
+block|}
+if|if
+condition|(
+name|info
+operator|.
+name|getPids
+argument_list|()
+operator|!=
 literal|null
+condition|)
+block|{
+name|pids1
+operator|.
+name|addAll
+argument_list|(
+name|Arrays
+operator|.
+name|asList
+argument_list|(
+name|info
+operator|.
+name|getPids
+argument_list|()
+argument_list|)
+argument_list|)
+expr_stmt|;
+block|}
+block|}
+return|return
+name|pids1
 return|;
 block|}
+argument_list|)
+return|;
 block|}
 block|}
 end_class

config/src/main/java/org/apache/karaf/config/core/impl/MetatypeCallable.java
@@ -1,55 +1,0 @@
-begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
-begin_comment
-comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
-end_comment
-
-begin_package
-package|package
-name|org
-operator|.
-name|apache
-operator|.
-name|karaf
-operator|.
-name|config
-operator|.
-name|core
-operator|.
-name|impl
-package|;
-end_package
-
-begin_import
-import|import
-name|org
-operator|.
-name|osgi
-operator|.
-name|service
-operator|.
-name|metatype
-operator|.
-name|MetaTypeService
-import|;
-end_import
-
-begin_interface
-specifier|public
-interface|interface
-name|MetatypeCallable
-parameter_list|<
-name|T
-parameter_list|>
-block|{
-name|T
-name|callWith
-parameter_list|(
-name|MetaTypeService
-name|metatypeService
-parameter_list|)
-function_decl|;
-block|}
-end_interface
-
-end_unit
-