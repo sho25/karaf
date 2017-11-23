sho25/karaf

commit 6ad5b6739bba07dfe2dba2e4158cfe0777d7ffcc

Primitives should not be boxed just for String conversion
Signed-off-by: Aurélien Pupier <apupier@redhat.com>

Former-commit-id: 9b9dd7947577dec377808344e78736988f7e8428

webconsole/gogo/src/main/java/org/apache/karaf/webconsole/gogo/Terminal.java
@@ -5408,13 +5408,16 @@ condition|)
 block|{
 name|prefix
 operator|=
-literal|""
-operator|+
+name|Character
+operator|.
+name|toString
+argument_list|(
 name|p
 operator|.
 name|charAt
 argument_list|(
 literal|0
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|p
@@ -7647,20 +7650,24 @@ condition|)
 block|{
 name|o
 operator|+=
-operator|(
-name|char
-operator|)
+name|Integer
+operator|.
+name|toString
+argument_list|(
 literal|8
+argument_list|)
 expr_stmt|;
 block|}
 else|else
 block|{
 name|o
 operator|+=
-operator|(
-name|char
-operator|)
+name|Integer
+operator|.
+name|toString
+argument_list|(
 literal|127
+argument_list|)
 expr_stmt|;
 block|}
 block|}
@@ -7668,7 +7675,12 @@ else|else
 block|{
 name|o
 operator|+=
+name|Integer
+operator|.
+name|toString
+argument_list|(
 name|c
+argument_list|)
 expr_stmt|;
 if|if
 condition|(
@@ -7681,10 +7693,12 @@ condition|)
 block|{
 name|o
 operator|+=
-operator|(
-name|char
-operator|)
+name|Integer
+operator|.
+name|toString
+argument_list|(
 literal|10
+argument_list|)
 expr_stmt|;
 block|}
 block|}

instance/src/main/java/org/apache/karaf/instance/main/Execute.java
@@ -1053,9 +1053,12 @@ throw|throw
 operator|new
 name|RuntimeException
 argument_list|(
-literal|""
-operator|+
+name|Integer
+operator|.
+name|toString
+argument_list|(
 name|rc
+argument_list|)
 argument_list|)
 throw|;
 block|}

shell/ssh/src/main/java/org/apache/karaf/shell/ssh/SshServerAction.java
@@ -412,14 +412,12 @@ name|SshServer
 operator|.
 name|NIO_WORKERS
 argument_list|,
-operator|new
 name|Integer
+operator|.
+name|toString
 argument_list|(
 name|nioWorkers
 argument_list|)
-operator|.
-name|toString
-argument_list|()
 argument_list|)
 expr_stmt|;
 comment|// welcome banner

commit a84fa02dbc02b467435066c28e5b31fa5fdbf4bc

Fix gogo webconsole plugin
Broken by 6ad5b6739bba07dfe2dba2e4158cfe0777d7ffcc [formerly 9b9dd7947577dec377808344e78736988f7e8428]


Former-commit-id: e57cb70539c12da8273e229b827e532bb47566f4

webconsole/gogo/src/main/java/org/apache/karaf/webconsole/gogo/Terminal.java
@@ -7640,7 +7640,7 @@ if|if
 condition|(
 name|c
 operator|==
-literal|127
+literal|'\u007f'
 condition|)
 block|{
 if|if
@@ -7650,24 +7650,14 @@ condition|)
 block|{
 name|o
 operator|+=
-name|Integer
-operator|.
-name|toString
-argument_list|(
-literal|8
-argument_list|)
+literal|'\u0008'
 expr_stmt|;
 block|}
 else|else
 block|{
 name|o
 operator|+=
-name|Integer
-operator|.
-name|toString
-argument_list|(
-literal|127
-argument_list|)
+literal|'\u007f'
 expr_stmt|;
 block|}
 block|}
@@ -7675,12 +7665,7 @@ else|else
 block|{
 name|o
 operator|+=
-name|Integer
-operator|.
-name|toString
-argument_list|(
 name|c
-argument_list|)
 expr_stmt|;
 if|if
 condition|(
@@ -7688,17 +7673,12 @@ name|vt100_mode_lfnewline
 operator|&&
 name|c
 operator|==
-literal|13
+literal|'\r'
 condition|)
 block|{
 name|o
 operator|+=
-name|Integer
-operator|.
-name|toString
-argument_list|(
-literal|10
-argument_list|)
+literal|'\n'
 expr_stmt|;
 block|}
 block|}