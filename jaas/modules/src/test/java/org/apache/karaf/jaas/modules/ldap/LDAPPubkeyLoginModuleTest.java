539bd975382d8475ad3001189975ab4ef3d16720
Jean-Baptiste Onofré <jbonofre@apache.org>
2017-12-10 11:24:39 +0100

    Fix missing and bad license headers
    Former-commit-id: 6ecfd47f51f8ac4fcbfe1838903c768b0b546af1

--- jaas/modules/src/test/java/org/apache/karaf/jaas/modules/ldap/LDAPPubkeyLoginModuleTest.java
+++ jaas/modules/src/test/java/org/apache/karaf/jaas/modules/ldap/LDAPPubkeyLoginModuleTest.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  * To change this license header, choose License Headers in Project Properties.  * To change this template file, choose Tools | Templates  * and open the template in the editor.  */
+comment|/*  *  Licensed under the Apache License, Version 2.0 (the "License");  *  you may not use this file except in compliance with the License.  *  You may obtain a copy of the License at  *  *       http://www.apache.org/licenses/LICENSE-2.0  *  *  Unless required by applicable law or agreed to in writing, software  *  distributed under the License is distributed on an "AS IS" BASIS,  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *  See the License for the specific language governing permissions and  *  limitations under the License.  *  under the License.  */
 end_comment
 
 begin_package
@@ -190,26 +190,6 @@ operator|.
 name|integ
 operator|.
 name|AbstractLdapTestUnit
-import|;
-end_import
-
-begin_import
-import|import static
-name|org
-operator|.
-name|apache
-operator|.
-name|directory
-operator|.
-name|server
-operator|.
-name|core
-operator|.
-name|integ
-operator|.
-name|AbstractLdapTestUnit
-operator|.
-name|getLdapServer
 import|;
 end_import
 
